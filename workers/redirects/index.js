c48681c7308f186b272c8a6c6bc407adcb4b02cb
Jacob Bolda <me@jacobbolda.com>
2020-05-08 00:59:25 -0500

    initial commit with redirect worker

---
+++ workers/redirects/index.js
@@ -0,0 +1,20 @@
+async function handleRequest(request) {
+  let requestURL = new URL(request.url)
+  let path = requestURL.pathname.replace(/\/+$/, '')
+  let location = redirectMap.get(path)
+  if (location) {
+    return Response.redirect(location, 301)
+  }
+  // If not in map, return the original request
+  return Response.redirect(`https://www.jacobbolda.com${path}/`, 301)
+}
+
+addEventListener('fetch', async event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const externalHostname = 'www.jacobbolda.com'
+const redirectMap = new Map([
+  ['/loan', 'https://' + externalHostname + '/loan-efficiency-calculator/'],
+  ['/gd', 'https://' + externalHostname + '/linking-data-in-gatsby/'],
+])
